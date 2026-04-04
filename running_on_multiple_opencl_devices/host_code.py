b4396dd80d7e76df8d006f7d7500ed7385f4a91a
khlkarim <karimkahili002@gmail.com>
2026-04-04 15:03:32 +0100

    setup boilerplate

--- running_on_multiple_opencl_devices/host_code.py
+++ running_on_multiple_opencl_devices/host_code.py
@@ -2,15 +2,13 @@ import numpy
 import pyopencl as cl
 from time import time
 
-WIDTH = 4
-
 # Number of matrix multiplication to perform
-COUNT = 20
+COUNT = 1
 
 # Matrix dimension
-N = 2048
-M = 2048
-K = 2048
+N = 8192
+M = N
+K = N
 sizeA = M * K
 sizeB = K * N
 sizeC = M * N
@@ -24,18 +22,10 @@ BVAL = 5.723
 cval = float(K) * AVAL * BVAL
 
 # OpenCL kernel
-kernel_name = "./matrix_multiplication_kernel_optimization/optimized_coalsced_kernel_1.cl"
+kernel_name = "./running_on_multiple_opencl_devices/default_uncoalsced_kernel.cl"
 
 # localsize ** 2 = work group size?
-kernel_size = input("Please enter a value for localsize. Possible values: 4, 8, 16 and 32: ")  # why is 32 the limit?
-
 localsize = 16
-if kernel_size in ["4", "8", "16", "32"]:
-    localsize = int(kernel_size)
-    print("Blocks Size is", localsize, "*", localsize)
-else:
-    print("=== No valid input. Default Size 16 will be used. Block Size = 16*16")
-
 
 # Set up OpenCL
 print("Creating an OpenCL context...")
@@ -68,8 +58,8 @@ for i in range(COUNT):
     try:
         mmul(
             queue,
-            (M // WIDTH, N),
-            (localsize // WIDTH, localsize),
+            (M, N),
+            (localsize, localsize),
             numpy.int32(M),
             numpy.int32(N),
             numpy.int32(K),
@@ -89,8 +79,3 @@ print(run_time, "seconds at", mflops, "MFLOPS")
 
 # Reading the result h_C
 cl.enqueue_copy(queue, h_C, d_c)
-
-# Check correctness of the operation
-"""for i in range(sizeC):
-    print(h_C[i], cval)
-    assert h_C[i] == cval"""
